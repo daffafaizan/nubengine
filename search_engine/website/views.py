2027805fe69b060f1f55062d56e2b36d8b70bb72
Daffa <daffamfzn@gmail.com>
2023-11-30 23:51:59 +0700

    feat: added search engine input and ability to see the query

--- search_engine/website/views.py
+++ search_engine/website/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def show_home(request):
+    context = {}
+
+    if request.method == 'POST':
+        query = request.POST.get('query')
+
+        if query is not None:
+            context['query'] = query
+
+            return render(request, 'home.html', context)
+        
     return render(request, 'home.html')
