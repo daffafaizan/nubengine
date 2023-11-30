fa33aba8c461834131b16a4c4a467000e17c16db
Daffa <daffamfzn@gmail.com>
2023-12-01 00:22:38 +0700

    feat: integrated search engine with front-end

--- search_engine/website/views.py
+++ search_engine/website/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
+from engine.bsbi import BSBIIndex
+from engine.compression import VBEPostings
 
 def show_home(request):
     context = {}
+    BSBI_instance = BSBIIndex(data_dir='../engine/collections',
+                            postings_encoding=VBEPostings,
+                            output_dir='../engine/index')
+    BSBI_instance.load()
 
     if request.method == 'POST':
-        query = request.POST.get('query')
+        queries_str = request.POST.get('queries')
 
-        if query is not None:
-            context['query'] = query
+        if queries_str:
+            queries = [query.strip() for query in queries_str.split('\n') if query.strip()]
+            results = []
+
+            for query in queries:
+                query_results = []
+                for (score, doc) in BSBI_instance.retrieve_tfidf(query, k=10):
+                    query_results.append({'doc': doc, 'score': score})
+
+                results.append({'query': query, 'results': query_results})
+
+            context['queries'] = queries
+            context['query_results'] = results
 
             return render(request, 'home.html', context)
-        
-    return render(request, 'home.html')
+
+    return render(request, 'home.html', context)
