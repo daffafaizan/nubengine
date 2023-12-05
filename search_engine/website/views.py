c0f7a6bbe3533188cb33d9915b4b5ab650f1a910
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2023-12-05 22:20:41 +0700

    beautify html

--- search_engine/website/views.py
+++ search_engine/website/views.py
@@ -9,27 +9,27 @@ def show_home(request):
                             postings_encoding=VBEPostings,
                             output_dir='../engine/index')
     BSBI_instance.load()
-    letor = Letor()
+    # letor = Letor()
 
-    if request.method == 'POST':
-        queries_str = request.POST.get('queries')
+    # if request.method == 'POST':
+    #     queries_str = request.POST.get('queries')
 
-        if queries_str:
-            queries = [query.strip() for query in queries_str.split('\n') if query.strip()]
-            results = []
+    #     if queries_str:
+    #         queries = [query.strip() for query in queries_str.split('\n') if query.strip()]
+    #         results = []
 
-            for query in queries:
-                query_results = []
-                tf_idf_result = BSBI_instance.retrieve_tfidf(query, k=10)
-                tf_idf_result = letor.rerank(query, [t[1] for t in tf_idf_result])
-                for (score, doc) in tf_idf_result:
-                    query_results.append({'doc': doc, 'score': score})
+    #         for query in queries:
+    #             query_results = []
+    #             tf_idf_result = BSBI_instance.retrieve_tfidf(query, k=10)
+    #             tf_idf_result = letor.rerank(query, [t[1] for t in tf_idf_result])
+    #             for (score, doc) in tf_idf_result:
+    #                 query_results.append({'doc': doc, 'score': score})
 
-                results.append({'query': query, 'results': query_results})
+    #             results.append({'query': query, 'results': query_results})
 
-            context['queries'] = queries
-            context['query_results'] = results
+    #         context['queries'] = queries
+    #         context['query_results'] = results
 
-            return render(request, 'home.html', context)
+    #         return render(request, 'home.html', context)
 
     return render(request, 'home.html', context)
